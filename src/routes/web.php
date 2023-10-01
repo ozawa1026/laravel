77799589471a2b184ece250e614b24958739f3bd
ozawa <omy.sk.izw.ss@gmail.com>
2023-10-01 13:18:10 +0900

    1

---
+++ src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\HelloController;
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index']);
